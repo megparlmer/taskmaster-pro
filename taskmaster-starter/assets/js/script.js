d4197bb492b04c5b3932cc7af83b23a3e183d362
megparlmer <megparlmer@gmail.com>
2022-03-08 08:43:42 -0500

    UX enhancement

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -188,7 +188,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -224,17 +224,19 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event) {
-    console.log("activate", this);
+  activate: function(event, ui) {
+    $(this).addClass(".dropover");
+    $(".bottom-trash").addClass(".bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass(".dropover");
+    $(".bottom-trash").removeClass(".bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", this);
+    $(event.target).addClass(".dropover-active");
   },
   out: function(event) {
-    console.log("out", this);
+    $(event.target).removeClass(".dropover-active");
   },
   update: function(event) {
     //array to store the task data in
@@ -274,14 +276,14 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
-    console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass(".bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass(".bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass(".bottom-trash-active");
   }
 
 });
@@ -293,3 +295,9 @@ $("#modalDueDate").datepicker({
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
